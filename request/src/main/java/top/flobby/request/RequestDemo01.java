7f2a3b657a1e9df5921e26b412940ccb8d76d1b7
yrb <lingtiannailad@qq.com>
2024-09-14 16:23:01 +0800

    first commit

---
+++ request/src/main/java/top/flobby/request/RequestDemo01.java
@@ -0,0 +1,16 @@
+package top.flobby.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class RequestDemo01 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
